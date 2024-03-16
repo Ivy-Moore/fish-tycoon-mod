e4fc22400d4945e68afc97ddfcce7d09f077b3ef
Ivy Moore <ivymmoore@gmail.com>
2024-03-16 16:08:47 -0500

    writing to the file, randomize working but obo and doesnt keep other file contents
    Committer Ivy Moore  <ivymmoore@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,27 @@
 extern crate rand;
 
 use rand::seq::SliceRandom;
-use std::fs::File;
-use std::io::{self, Read};
-use zip::read::ZipArchive;
+use std::fs::{self, File};
+use std::io::{self, Read, Write};
+use zip::write::FileOptions;
+use zip::write::ZipWriter;
 
-fn main() -> io::Result<()> {
+fn zippy() -> io::Result<()> {
     // List of fish species names
-    let fish_species = [
-        "Angelfish",
-        "Betta",
-        "Clownfish",
-        "Dorado",
-        "Eel",
-        "Flounder",
-        "Goldfish",
-        "Haddock",
-        "Icefish",
-        "Jellyfish",
-        "Koi",
-        "Lionfish",
-        "Mackerel",
-        "Neon Tetra",
-        "Octopus",
-        "Pufferfish",
-        "Quillback",
-        "Rainbowfish",
-        "Swordfish",
-        "Tuna",
-    ];
+    let mut fish_species = Vec::new();
+    for i in 0..20 {
+        fish_species.push(format!("fish_{:02}_body_grid.png", i));
+    }
 
     // Shuffle the fish species names randomly
     let mut rng = rand::thread_rng();
+    let shuffled_indices: Vec<usize> = (0..=20).collect();
     let shuffled_species: Vec<_> = fish_species.choose_multiple(&mut rng, 20).collect();
 
     // Print the randomized fish species names
     println!("Randomized Fish Species:");
-    for species in &shuffled_species {
-        println!("{}", species);
+    for (index, species) in shuffled_species.iter().enumerate() {
+        println!("{} -> {}", species, fish_species[shuffled_indices[index]]);
     }
 
     // Read the .dat file
@@ -45,17 +29,56 @@ fn main() -> io::Result<()> {
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer)?;
 
+    // Create a temporary file to write the modified contents
+    let mut temp_file = File::create("src/ft_temp.dat")?;
+    temp_file.write_all(&buffer)?;
+
     // Unzip the file
-    let mut archive = ZipArchive::new(io::Cursor::new(buffer))?;
-    let mut fish_png_file = archive.by_name("fish_20_body_grid.png")?;
+    let mut archive = ZipWriter::new(temp_file);
 
-    // Read the contents of the fish.png file
-    let mut png_content = Vec::new();
-    fish_png_file.read_to_end(&mut png_content)?;
+    // Iterate over each fish species and read the corresponding file
+    for (index, species) in shuffled_species.iter().enumerate() {
+        println!("BBBB 0");
+        let zip_file = File::open("src/ft.dat")?;
+        println!("BBBB 1");
 
-    // At this point, `png_content` contains the content of the fish.png file
-    println!("Successfully read fish.png from the .dat file.");
+        println!("idx: {}, species: {}", index, species);
+
+        println!("0");
+        let mut zip_archive = zip::read::ZipArchive::new(zip_file)?;
+        println!("1");
+        let mut fish_png_file = zip_archive.by_name(species)?;
+        // Read the contents of the fish.png file
+        let mut png_content = Vec::new();
+        println!("AAA 0");
+        fish_png_file.read_to_end(&mut png_content)?;
+        println!("AAA 1");
+
+        // Write the contents to the new file with the randomized name
+        let options = FileOptions::default()
+            .compression_method(zip::CompressionMethod::Stored)
+            .unix_permissions(0o755);
+        println!("AAA 2");
+        archive.start_file(&fish_species[shuffled_indices[index]], options)?;
+        archive.write_all(&png_content)?;
+        println!("AAA 3");
+
+        println!("Successfully wrote {} to the .dat file.", &fish_species[shuffled_indices[index]]);
+        //} else {
+        //    println!("Failed to read {} from the .dat file.", species);
+        //}
+    }
+
+    archive.finish()?;
+
+    // Replace the original ft.dat with the modified file
+    fs::rename("src/ft_temp.dat", "src/ft.dat")?;
 
     Ok(())
 }
 
+
+
+fn main() {
+    zippy().unwrap();
+}
